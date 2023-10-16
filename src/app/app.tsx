161bac8e116bd8d61e8b70866cd097844fe8bf0c
strwatcher <starwatcher.focused@gmail.com>
2023-10-16 18:16:27 +1000

    combine app components in app.tsx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,18 @@
 import { RouterProvider } from "atomic-router-react";
 import { router } from "./router";
+import { Pages } from "@/pages";
+import { MantineProvider } from "@mantine/core";
+import { useUnit } from "effector-react";
+import { $pending } from "@/shared/config/pending";
+import { Loader } from "@/shared/ui";
 export const App = () => {
+  const pending = useUnit($pending);
   return (
     <RouterProvider router={router}>
-      <>Hello</>
+      <MantineProvider withCssVariables>
+        <Loader visible={pending} />
+        <Pages />
+      </MantineProvider>
     </RouterProvider>
   );
 };
