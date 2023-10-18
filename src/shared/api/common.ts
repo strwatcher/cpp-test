f2df97753f73a1492cdac54b4cb7ed9c48a2edf8
strwatcher <starwatcher.focused@gmail.com>
2023-10-19 01:25:27 +1000

    fix default url in combine url

--- src/shared/api/common.ts
+++ src/shared/api/common.ts
@@ -2,7 +2,7 @@ type CombineUrlParams = { resource: string; other?: string; base?: string };
 
 export function combineUrl({ resource, other, base }: CombineUrlParams) {
   const baseUrl =
-    base ?? import.meta.env.VITE_API_BASE_URL ?? "http://localhost/31299";
+    base ?? import.meta.env.VITE_API_BASE_URL ?? "http://localhost/31299/";
 
   return `${baseUrl}${resource}${other ? `/${other}` : ""}`;
 }
